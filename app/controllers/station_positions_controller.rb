501b65780945a0a0b74a979ebb706454165f6b66
山田大地 <private@threeaster.net>
2015-04-18 19:36:05 +0900

    htmlをrails化

--- app/controllers/station_positions_controller.rb
+++ app/controllers/station_positions_controller.rb
@@ -2,4 +2,8 @@ class StationPositionsController < ApplicationController
   def index
 
   end
+
+  def map01
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'map01', to: 'station_positions#map01'
   root 'station_positions#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
